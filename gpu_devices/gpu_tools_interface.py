75f9a20b9bcb9fb5dde473c26f78044370f5a112
JohnAtGrayAI <john@grayai.com>
2024-02-02 12:42:19 -0800

    added gpu utilization metric to nvidia.

--- gpu_devices/gpu_tools_interface.py
+++ gpu_devices/gpu_tools_interface.py
@@ -15,3 +15,5 @@ class DiagnosticCLI:
 if __name__ == """__main__""":
     parser = argparse.ArgumentParser()
     parser.add_argument("-h", type=str)
+    parser.add_argument("-w", type=str)
+    args = parser.parse_args()
